5144687830713a48c1f26397f885b2251894945e
Piotr Prokopowicz <git@nes.slmail.me>
2025-12-07 10:33:04 +0100

    Day07 part 2 implemented

--- src/day07/day07.zig
+++ src/day07/day07.zig
@@ -33,7 +33,7 @@ pub fn solve(allocator: std.mem.Allocator) !void {
     std.debug.print("Day 07:\n", .{});
 
     try solve_part1(manifold, allocator);
-    solve_part2();
+    try solve_part2(manifold, allocator);
 
     std.debug.print("\n", .{});
 }
@@ -78,8 +78,67 @@ fn solve_part1(manifold: Manifold, allocator: std.mem.Allocator) !void {
     std.debug.print("\tPart 1: {}\n", .{sum});
 }
 
-fn solve_part2() void {
-    // std.debug.print("\tPart 2: {}\n", .{sum});
+fn solve_part2(manifold: Manifold, allocator: std.mem.Allocator) !void {
+    var beam_coordinates = std.AutoHashMap(Coordinates, usize).init(allocator);
+    defer beam_coordinates.deinit();
+
+    try beam_coordinates.put(manifold.start, 1);
+
+    for (manifold.diagram, 0..) |locations, y| {
+        var next_beam_coordinates = std.AutoHashMap(Coordinates, usize).init(allocator);
+        var it = beam_coordinates.keyIterator();
+
+        while (it.next()) |coordinate| {
+            const x = coordinate.x;
+            const next_position = locations[x];
+            const current_amount = beam_coordinates.get(coordinate.*).?;
+
+            switch (next_position) {
+                .empty => {
+                    const next = Coordinates{ .x = x, .y = y };
+
+                    if (next_beam_coordinates.contains(next)) {
+                        const next_amount = next_beam_coordinates.get(next).?;
+                        try next_beam_coordinates.put(next, current_amount + next_amount);
+                    } else {
+                        try next_beam_coordinates.put(next, current_amount);
+                    }
+                },
+                .splitter => {
+                    const left = Coordinates{ .x = x - 1, .y = y };
+                    const right = Coordinates{ .x = x + 1, .y = y };
+
+                    if (next_beam_coordinates.contains(left)) {
+                        const left_amount = next_beam_coordinates.get(left).?;
+
+                        try next_beam_coordinates.put(left, current_amount + left_amount);
+                    } else {
+                        try next_beam_coordinates.put(left, current_amount);
+                    }
+
+                    if (next_beam_coordinates.contains(right)) {
+                        const right_amount = next_beam_coordinates.get(right).?;
+
+                        try next_beam_coordinates.put(right, current_amount + right_amount);
+                    } else {
+                        try next_beam_coordinates.put(right, current_amount);
+                    }
+                },
+            }
+        }
+
+        beam_coordinates.deinit();
+        beam_coordinates = next_beam_coordinates;
+    }
+
+    var sum: usize = 0;
+
+    var it = beam_coordinates.valueIterator();
+    while (it.next()) |value| {
+        sum += value.*;
+    }
+
+    std.debug.print("\tPart 2: {}\n", .{sum});
 }
 
 fn parse_content(content: []u8, allocator: std.mem.Allocator) !Manifold {
